a1a3f632f5085b5cf2e9df26cef8d7874da716dc
ngushteraklieva <ngushteraklieva@gmail.com>
2026-02-11 23:12:35 +0100

    Filter by brand and type, sort by price

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Runtime.CompilerServices;
 using Core.Entities;
 
 namespace Core.Interfaces;
 
 public interface IProductRepository
 {
-    Task<IReadOnlyList<Product>> GetProductsAsync();
+    Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort);
     Task<Product?> GetProductByIdAsync(int id);
     Task<IReadOnlyList<string>> GetBrandsAsync();
     Task<IReadOnlyList<string>> GetTypesAsync();
