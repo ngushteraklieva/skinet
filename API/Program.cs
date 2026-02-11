5b9489f74132f2e08730353ebf31104be7e03237
ngushteraklieva <ngushteraklieva@gmail.com>
2026-02-11 17:25:26 +0100

    Seed Data

--- API/Program.cs
+++ API/Program.cs
@@ -32,4 +32,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.Seedasync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
+
 app.Run();
