a3a5584e294fc672c86c593788142469fc769ff7
ngushteraklieva <ngushteraklieva@gmail.com>
2026-03-06 14:47:42 +0100

    Implement ShoppingCart logic

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
 using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +23,14 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 builder.Services.AddCors();
+builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Redis")
+        ?? throw new Exception("Cannot get redis connection string");
+    var configuation = ConfigurationOptions.Parse(connectionString, true);
+    return ConnectionMultiplexer.Connect(configuation);
+});
+builder.Services.AddSingleton<ICartService, CartService>();
 
 var app = builder.Build();
 
