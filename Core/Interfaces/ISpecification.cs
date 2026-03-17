be8e193f1ebf5b7fd9f99f2a2dbf108bf32584f3
ngushteraklieva <ngushteraklieva@gmail.com>
2026-03-17 11:34:54 +0100

    Update specs for eager loading

--- Core/Interfaces/ISpecification.cs
+++ Core/Interfaces/ISpecification.cs
@@ -9,6 +9,8 @@ public interface ISpecification<T>
     Expression<Func<T, bool>>? Criteria {get;}
     Expression<Func<T, object>>? OrderBy {get;}
     Expression<Func<T, object>>? OrderByDescending {get;}
+    List<Expression<Func<T, object>>> Includes {get;}
+    List<string> IncludesString {get;}
     bool IsDistinct {get;}
     int Take {get;}
     int Skip {get;}
--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -23,6 +23,10 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
 
     public bool IsPagingEnabled { get; private set; }
 
+    public List<Expression<Func<T, object>>> Includes {get;} = [];
+
+    public List<string> IncludesString {get;} = [];
+
     public IQueryable<T> ApplyCriteria(IQueryable<T> query)
     {
         if(Criteria != null)
@@ -31,6 +35,16 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
         };
          
         return query;
+    }
+
+    protected void AddInclude(Expression<Func<T, object>> includeExpression)
+    {
+        Includes.Add(includeExpression);
+    }
+
+    protected void AddInclude(string includeString)
+    {
+        IncludesString.Add(includeString);
     }
 
     protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
