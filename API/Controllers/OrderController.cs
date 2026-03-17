c1eacfc257aeb16c4915d4be2ed84eacbcc8f195
ngushteraklieva <ngushteraklieva@gmail.com>
2026-03-17 11:05:08 +0100

    Get orders

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -73,4 +73,20 @@ public class OrdersController(ICartService cartService, IUnitOfWork unit) : Base
         return BadRequest("Problem creating order");
     }
 
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Order>>> GetOrdersForUser()
+    {
+        var spec = new OrderSpecification(User.GetEmail());
+        var orders = await unit.Repository<Order>().ListAsync(spec);
+        return Ok(orders);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Order>> GetOrderById(int id)
+    {
+        var spec = new OrderSpecification(User.GetEmail(), id);
+        var order = await unit.Repository<Order>().GetEntityWithSpec(spec);
+        if (order == null) return NotFound();
+        return order;
+    }
 }
