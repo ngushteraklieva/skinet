6a51168edbda313a48460e75fbc3837367a973c2
ngushteraklieva <ngushteraklieva@gmail.com>
2026-03-17 17:26:59 +0100

    Display orders

--- client/src/app/features/orders/order.component.ts
+++ client/src/app/features/orders/order.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../core/services/order.service';
+import { RouterLink } from '@angular/router';
+import { Order } from '../../shared/models/order';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-order',
-  imports: [],
+  imports: [RouterLink, CurrencyPipe, DatePipe],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css',
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  private orderService = inject(OrderService)
+  orders: Order[] = []
 
+  ngOnInit(): void {
+    this.orderService.getOrdersForUser().subscribe({
+      next: orders => this.orders = orders
+    })
+  }
 }
